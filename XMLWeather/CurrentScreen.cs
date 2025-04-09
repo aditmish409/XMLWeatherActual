834926634da3df2febb4f58bcefcf25b98aa7b1c
aditmish409 <aditmish409@SDS-L-262Q00>
2025-04-09 14:34:12 -0400

    Add project files.

---
+++ XMLWeather/CurrentScreen.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace XMLWeather
+{
+    public partial class CurrentScreen : UserControl
+    {
+
+        public CurrentScreen()
+        {
+            InitializeComponent();
+            DisplayCurrent();
+        }
+
+        public void DisplayCurrent()
+        {
+            cityOutput.Text = Form1.days[0].location;
+            minOutput.Text = Form1.days[0].tempLow;
+            maxOutput.Text = Form1.days[0].tempHigh;
+            currentTempOutput.Text = Form1.days[0].currentTemp;
+            dateLabel.Text = DateTime.Now.ToString("dddd M");
+        }
+
+        private void forecastLabel_Click(object sender, EventArgs e)
+        {
+            Form f = this.FindForm();
+            f.Controls.Remove(this);
+
+            ForecastScreen fs = new ForecastScreen();
+            f.Controls.Add(fs);
+        }
+    }
+}
